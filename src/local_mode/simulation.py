1334493023be7d79d2db693e1dfe9953dddcea0d
Samuel Giret <samuel.giret@epitech.eu>
2025-04-14 14:41:53 +0200

    [ADD] Define Sphere and Torus classes with initialization and classname attributes

--- src/local_mode/simulation.py
+++ src/local_mode/simulation.py
@@ -12,6 +12,33 @@ MAX_STEPS = 1000  # Maximum number of steps for ray marching
 MAX_DISTANCE = 1000.0  # Maximum distance to consider for ray marching
 MIN_DISTANCE = 0.1  # Distance threshold for intersection
 
+#################################################################################
+
+class Sphere:
+    def __init__(self, x, y, z, radius):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.radius = radius
+        self.classname = "Sphere"
+
+class Torus:
+    def __init__(self, x, y, z, inRadius, outRadius):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.inRadius = inRadius
+        self.outRadius = outRadius
+        self.classname = "Torus"
+
+def local(bodies, argv):
+    # print(bodies)
+    print(
+        f"Rock thrown at the point ({float(argv[3])}, {float(argv[4])}, {float(argv[5])}) "
+        f"and parallel to the vector ({float(argv[6])}, {float(argv[7])}, {float(argv[8])})"
+    )
+
+###################################################################################################
 
 def run_local_simulation(bodies, position, velocity):
     """
